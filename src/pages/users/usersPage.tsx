d3b9e39662e2c5a3162e6d1fc0a07de7333a5345
kappafon <goran.rihter@gmail.com>
2020-05-30 11:55:51 +0200

    Cursor based users pagination

--- src/pages/users/usersPage.tsx
+++ src/pages/users/usersPage.tsx
@@ -7,7 +7,7 @@ import './usersPage.scss'
 import Loading from '../../components/loading/loading'
 
 const UsersPage: React.FunctionComponent = () => {
-    const [getUsers, { loading, data, error }] = useLazyQuery(GET_USERS)
+    const [getUsers, { loading, data, error, fetchMore }] = useLazyQuery(GET_USERS)
     const params = useParams()
     const value = params.value
 
@@ -16,20 +16,49 @@ const UsersPage: React.FunctionComponent = () => {
     }, [value])
 
     if (loading) return <Loading />
-    if (error) return <p>Error</p>
+    if (error) return <p>{error.message}</p>
 
     const users = data && data.search && data.search.edges ? data.search.edges : []
+
+    const loadMore = () => {
+        console.log('loadingMore')
+        fetchMore({
+            variables: {
+                after: data.search.pageInfo.endCursor,
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+                console.log('loadMore -> prev', prev)
+                if (!fetchMoreResult) return prev
+                console.log('loadMore -> fetchMoreResult', fetchMoreResult)
+                return {
+                    ...fetchMoreResult,
+                    search: {
+                        ...fetchMoreResult.search,
+                        edges: [...prev.search.edges, ...fetchMoreResult.search.edges],
+                    },
+                }
+            },
+        })
+    }
+
     return (
         <div className="users__container">
-            {users.length === 0 ? <div>no result</div> : <Users users={users} />}
+            {users.length === 0 ? (
+                <div>no result</div>
+            ) : (
+                <>
+                    {data.search.pageInfo.hasNextPage && <div onClick={loadMore}>load more</div>}
+                    <Users users={users} />
+                </>
+            )}
         </div>
     )
 }
 export default UsersPage
 
 const GET_USERS = gql`
-    query user($user: String!) {
-        search(query: $user, type: USER, first: 100) {
+    query user($user: String!, $after: String) {
+        search(query: $user, type: USER, first: 3, after: $after) {
             userCount
             edges {
                 node {
@@ -39,6 +68,11 @@ const GET_USERS = gql`
                         avatarUrl
                     }
                 }
+                cursor
+            }
+            pageInfo {
+                hasNextPage
+                endCursor
             }
         }
     }
