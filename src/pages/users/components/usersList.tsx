bb7aef2c227bf7d7af846ef8bcad1914f1f26857
kappafon <goran.rihter@gmail.com>
2020-06-03 19:59:04 +0200

    Missed 1 href

--- src/pages/users/components/usersList.tsx
+++ src/pages/users/components/usersList.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { USERS_PAGE } from '../../../assets/strings/strings'
 import './usersList.scss'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 //#region Interfaces
 export interface UsersListProps {
     users: Array<UsersInfo>
@@ -20,8 +22,11 @@ const UsersList: React.FunctionComponent<UsersListProps> = (props) => {
             {users.map((user) => {
                 const { login, avatarUrl } = user
                 const alt = `${login}${userImageAlt}`
+                const href = isProduction
+                    ? `/github-user-search/profile/${login}`
+                    : `/profile/${login}`
                 return (
-                    <Link key={login} to={'/profile/' + login} className="user__box">
+                    <Link key={login} to={href} className="user__box">
                         <img className="user__image" src={avatarUrl} alt={alt} width={128} />
                         <div className="user__username">{login}</div>
                     </Link>
